de8606df3f4f29515f21fffaced8960747cf862a
LuvMySenpai <stop.this.shit@mail.ru>
2024-05-20 19:52:35 +0400

    -

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
 export const BASE_URL = 'https://coolpindiebackend.nomoredomainswork.ru/api';
 
 export const endpoints = {
-  games: `https://coolpindiebackend.nomoredomainswork.ru/api/games`,
+  games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
   reg: `${BASE_URL}/users`,
   me: `${BASE_URL}/me`,
