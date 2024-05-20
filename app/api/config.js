901f5c8a5ad99b91ffd0671e7b8a3ee753b42d06
LuvMySenpai <stop.this.shit@mail.ru>
2024-05-20 19:29:49 +0400

    fixed registration endpoint

--- app/api/config.js
+++ app/api/config.js
@@ -3,6 +3,6 @@ export const BASE_URL = 'https://coolpindiebackend.nomoredomainswork.ru/api';
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
-  reg: `${BASE_URL}/auth/local/register`,
+  reg: `${BASE_URL}/users`,
   me: `${BASE_URL}/me`,
 };
